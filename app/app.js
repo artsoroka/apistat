8875c23845a47efbbedcb10ed632447237d01d49
artsoroka <githubrepository@yandex.ru>
2015-08-08 21:47:15 +0000

    api usage statistics http server

--- app/app.js
+++ app/app.js
@@ -4,8 +4,6 @@ var config = require("./config");
 var db     = require("./mysql")(config.db); 
 
 var proxyServer = http.createServer(function(req,res){
-    console.log(req.url);
-    console.log(req.method);
     
     var request = req.url.split('?'); 
     var path    = request[0]; 
@@ -14,7 +12,8 @@ var proxyServer = http.createServer(function(req,res){
     db.query('INSERT INTO api_usage SET ?', {
 		method: req.method, 
 		path: path, 
-		query: query 
+		query: query, 
+		timestamp: parseInt(Date.now() / 1000) 
 	}, function(err, info){
 		if( err ) return console.log('db err: ', err); 
 	});
@@ -30,3 +29,11 @@ var server = http.createServer(function(req, res) {
 
 server.listen(9000); 
 
+var reports = http.createServer(function(req,res){
+   db.query('SELECT method, path, count(id) as hits FROM api_usage GROUP BY path', function(err, data){
+       if( err ) return res.end('db error'); 
+       res.end(JSON.stringify(data)); 
+   });  
+}); 
+
+reports.listen(9001); 
