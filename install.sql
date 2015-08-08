004b8295727f2d7606d91a62d605fb3b61d43750
artsoroka <githubrepository@yandex.ru>
2015-08-08 21:36:09 +0000

    request query can be empty

--- install.sql
+++ install.sql
@@ -3,6 +3,6 @@ CREATE TABLE `api_usage` (
   id int auto_increment PRIMARY KEY, 
   method varchar(10) NOT NULL, 
   path  varchar(255) NOT NULL, 
-  query varchar(255) NOT NULL, 
+  query varchar(255),  
   timestamp int NOT NULL 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
